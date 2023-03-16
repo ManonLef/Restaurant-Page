de69b6bdb3d3c7d3c76ad846dd6130c2dd449a58
Manon <manonlefkos@gmail.com>
2023-03-16 16:04:41 +0100

    add logo to header and header styling

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,22 +5,18 @@ function homePageLoader() {
   tabLoader();
   
   document.querySelector(".home-tab").className = "home-tab active-page"
-  
-  const restaurantName = document.createElement("div");
-  restaurantName.className = "restaurant-name";
-  restaurantName.textContent = "T H E • J O L L Y • J U I C E R";
+
 
   const heroImage = new Image();
   heroImage.src = peachImage;
   heroImage.alt = "peaches on a table";
-  heroImage.width = "500";
 
   const welcomeMessage = document.createElement("p");
   welcomeMessage.className = "welcome";
   welcomeMessage.textContent =
     "Welcome to The Jolly Juicer! Our juice bar is the perfect place to grab a tasty and healthy drink made with locally sourced and organic fruits and veggies. Come in today and try one of our signature blends or create your own custom juice.";
 
-  document.querySelector("#content").append(restaurantName, heroImage, welcomeMessage);
+  document.querySelector("#content").append( heroImage, welcomeMessage);
 
 }
 
--- src/tabs.js
+++ src/tabs.js
@@ -3,13 +3,16 @@ import { clearContents } from "./clearContents";
 
 function tabLoader() {
   clearContents();
-  
+
   const header = document.createElement("header");
+  const restaurantName = document.createElement("div")
   const tabs = document.createElement("ul");
   const home = document.createElement("li");
   const menu = document.createElement("li");
   const contact = document.createElement("li");
 
+  restaurantName.textContent = "T H E • J O L L Y • J U I C E R"
+  restaurantName.className = "logo"
   home.textContent = "home";
   home.className = "home-tab"
   menu.textContent = "menu";
@@ -17,7 +20,9 @@ function tabLoader() {
   contact.textContent = "contact";
   contact.className = "contact-tab"
 
+  header.appendChild(restaurantName)
   header.appendChild(tabs);
+ 
   tabs.append(home, menu, contact)
 
   document.querySelector("#content").appendChild(header);
