cec951c7d3369fbe93894f881d7d70d1344573a8
Manon <manonlefkos@gmail.com>
2023-03-16 12:52:38 +0100

    add clearcontents function

--- src/home.js
+++ src/home.js
@@ -1,5 +1,6 @@
 import peachImage from "./peachtable.jpg";
 import { tabLoader } from "./tabs";
+import { clearContents } from "./clearContents";
 
 function homePageLoader() {
   tabLoader();
@@ -19,6 +20,7 @@ function homePageLoader() {
     "Welcome to The Jolly Juicer! Our juice bar is the perfect place to grab a tasty and healthy drink made with locally sourced and organic fruits and veggies. Come in today and try one of our signature blends or create your own custom juice.";
 
   document.querySelector("#content").append(restaurantName, heroImage, welcomeMessage);
+
 }
 
 export { homePageLoader };
