4e3f3321196d8172159657c5888c769f0a4e5b52
Manon <manonlefkos@gmail.com>
2023-03-16 23:08:06 +0100

    update gitignore for dist

--- src/clearContents.js
+++
@@ -1,9 +0,0 @@
-function clearContents() {
-  const container = document.querySelector("#content");
-  
-  while (container.firstChild) {
-    container.removeChild(container.firstChild);
-  }
-}
-
-export { clearContents };
--- src/footer.js
+++
@@ -1,7 +0,0 @@
-function addFooter() {
-  const footer = document.createElement("footer");
-  footer.textContent = "© Manon Lef 2023";
-  document.querySelector("#content").appendChild(footer);
-}
-
-export { addFooter };
