cbbe742f9a0a672b675052e30e16117bdb1a6f02
Manon <manonlefkos@gmail.com>
2023-03-16 10:48:24 +0100

    finish setup homePageLoad

--- src/home.js
+++ src/home.js
@@ -1,18 +1,14 @@
-/* <div class="dummy-content">
-    <div class="header">T H E • J O L L Y • J U I C E R</div>
-    <img src="../peachtable.jpg" alt="peaches on a table" width="100%">
-    <p class="welcome">Welcome to The Jolly Juicer! Our juice bar is the perfect place to grab a tasty and healthy drink made with locally sourced and organic fruits and veggies. Come in today and try one of our signature blends or create your own custom juice.</p>
-  </div> */
+import peachImage from "./peachtable.jpg";
 
 function homePageLoad() {
   const restaurantName = document.createElement("div");
   restaurantName.className = "restaurant-name";
   restaurantName.textContent = "T H E • J O L L Y • J U I C E R";
 
-  const heroImage = document.createElement("img");
-  heroImage.src = "../peachtable.jpg";
+  const heroImage = new Image();
+  heroImage.src = peachImage;
   heroImage.alt = "peaches on a table";
-  heroImage.width = "100%";
+  heroImage.width = "500";
 
   const welcomeMessage = document.createElement("p");
   welcomeMessage.className = "welcome";
